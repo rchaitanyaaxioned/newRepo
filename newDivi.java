408e4cc820f6facd0f0e22ccc17727b2de3c41bb
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-06-08 10:49:19 +0530

    Commit after adding newDivi.java and newModu.java

---
+++ newDivi.java
@@ -0,0 +1,13 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Division of two numbers");
+    float num1 = 2000f;
+    float num2 = 150f;
+
+    // divide two numbers
+    float divi = num1 / num2;
+    System.out.println(divi);
+  }
+}
---
+++ newModu.java
@@ -0,0 +1,13 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Modulus of two numbers");
+    float num1 = 2000f;
+    float num2 = 150f;
+
+    // modulus of two numbers
+    float modu = num1 / num2;
+    System.out.println(modu);
+  }
+}
