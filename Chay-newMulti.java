b07b5d3a5c603a4a2d6c481238d7d574d013620f
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-06-08 10:39:14 +0530

    Commit after adding Chay-newMulti

---
+++ Chay-newMulti.java
@@ -0,0 +1,13 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Multiplication of two numbers");
+    int num1 = 200;
+    int num2 = 15;
+
+    // add two numbers
+    int multi = num1 * num2;
+    System.out.println(multi);
+  }
+}
