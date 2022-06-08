5fe4e6d659e36cd23726fe19cdad790cf916ee84
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-06-08 10:11:16 +0530

    newAdd intial commit

---
+++ newAdd.java
@@ -0,0 +1,13 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Addition of two numbers");
+    int num1 = 200;
+    int num2 = 150;
+
+    // add two numbers
+    int sum = num1 + num2;
+    System.out.println(sum);
+  }
+}
