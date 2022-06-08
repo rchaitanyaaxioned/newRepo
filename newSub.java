8c01d77f011c28bcbffb5de3ac749837bc913349
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-06-08 10:17:53 +0530

    Commit after modifying newAdd.java and adding newSub.java

---
+++ newSub.java
@@ -0,0 +1,13 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Subtraction of two numbers");
+    int num1 = 200;
+    int num2 = 150;
+
+    // add two numbers
+    int sub = num1 - num2;
+    System.out.println(sub);
+  }
+}
